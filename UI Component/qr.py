07c7b7d1174740028fe23de5e54fed79f6dfe363
cksdn06 <128167545+cksdn06@users.noreply.github.com>
2023-10-16 20:38:50 +0900

    Update qr.py

--- UI Component/qr.py
+++ UI Component/qr.py
@@ -1,1 +1,5 @@
+import qrcode
 
+img_url = qrcode.make("https://drive.google.com/file/d/1_vp6fNGR2gFjfuewSK3kWiUKJjgF7qwH/view?usp=sharing")
+img_url.save("C:\\Users\\User\\Desktop\\111\\output.jpg")
+print(img_url.size)
