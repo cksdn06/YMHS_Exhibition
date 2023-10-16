64850eaadfded16000b55ec01cfb94006963ab25
cksdn06 <128167545+cksdn06@users.noreply.github.com>
2023-10-16 20:38:58 +0900

    Delete qr.py

--- qr.py
+++
@@ -1,5 +0,0 @@
-import qrcode
-
-img_url = qrcode.make("https://drive.google.com/file/d/1_vp6fNGR2gFjfuewSK3kWiUKJjgF7qwH/view?usp=sharing")
-img_url.save("C:\\Users\\User\\Desktop\\111\\output.jpg")
-print(img_url.size)
