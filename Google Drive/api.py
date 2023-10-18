38a8816c7993954bb50d189047cd4317bca17c22
cksdn06 <128167545+cksdn06@users.noreply.github.com>
2023-10-18 18:19:25 +0900

    Delete Google Drive directory

--- Google Drive/api.py
+++
@@ -1,9 +0,0 @@
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
-
-gauth = GoogleAuth()
-drive = GoogleDrive(gauth)
-
-file1 = drive.CreateFile({'title': 'output.jpg', 'parents': [{'id': '1CBFFj7OqV9jC8htkotR6ytYYgnmPvmPw'}]}) 
-file1.SetContentFile('C:\\tf\\output.jpg') 
-file1.Upload()
